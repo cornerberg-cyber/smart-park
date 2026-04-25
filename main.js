4fe7e4116c01a133748fdc0868ae48aa1de1fe85
Andreas <andre@example.com>
2026-04-25 13:06:12 +0200

    Add PWA support

--- main.js
+++ main.js
@@ -225,3 +225,14 @@ function populateResult(res) {
 
 // Start
 init();
+
+// Register Service Worker for PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
